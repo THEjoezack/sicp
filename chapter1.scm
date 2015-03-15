6102c97c47cbff1325951b416f59fa8268e78395
THEjoezack <me@joezack.com>
2015-03-14 22:52:59 -0400

    Shorter description for 1.16

--- chapter1.scm
+++ chapter1.scm
@@ -106,7 +106,7 @@
 (assert 6 (pascal 4 2))
 
 ;;; Excercise 1.16
-;;; Design a procedure that evolves an iterative exponentiation process that uses successive squaring and uses a logarithmic number of steps, as does fast-expt
+;;; Iterative exponentiation using successive squaring
 
 (define (even? n)
   (= (remainder n 2) 0))
